c7f4f2d82cefb2f80cfa36168652ff369a20f374
shyam kumawat <119164326+shyamkmt@users.noreply.github.com>
2024-01-02 05:14:58 +0000

    Datatypes

---
+++ v05.js
@@ -0,0 +1,30 @@
+"use strict";   // treat all js code as a new version 
+
+//alert(44+5);  // we r using nodejs not browser thats why showing error
+
+console.log(4-
+    
+    8 /
+    
+    2);  // code readability is high priority
+
+// DATATYPES
+
+let name = "shyam";                   //String
+let age = 30;                         // number  
+let bigNum = 4748474633788383849n;    // big number
+let isAnimal= true;                   // boolean
+let cast = null;                      // null (standalone value)
+let prof;                             // undefined
+// object
+ 
+console.log([name, age, bigNum, isAnimal, cast, prof]);
+console.table([name, age, bigNum, isAnimal, cast, prof]);
+
+console.log(typeof name);
+console.log(typeof cast);
+console.log(typeof prof);
+
+
+
+
