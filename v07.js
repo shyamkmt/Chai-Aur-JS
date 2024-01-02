35eb19690ff1e19075c81462aa638db50cd04a34
shyam kumawat <119164326+shyamkmt@users.noreply.github.com>
2024-01-02 07:27:55 +0000

    Pre Postfix

---
+++ v07.js
@@ -0,0 +1,16 @@
+let value = 4;
+let negValue = -value;
+console.log(negValue, typeof negValue);
+
+console.log(+true);
+console.log(-true);
+
+// *********************** PREFIX AND POSTFIX ***********************
+
+let score = 11;
+console.log(score++);
+console.log(score);
+
+let num = 3;
+console.log(++num);
+console.log(num);
